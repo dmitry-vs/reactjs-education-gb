f4a27754b04bb7050d81ef7c8fea4b619a9d3090
dmitry-vs <dmitryselin@inbox.ru>
2018-10-29 17:52:14 +0300

    Webpack build ok

---
+++ src/app.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import Blog from './app/components/blog';
+// import BlogConfig from './app/components/config';
+
+// ReactDOM.render(<Blog featuredPosts={BlogConfig.featuredPosts} posts={BlogConfig.posts}/>, document.body);
+
+ReactDOM.render(<h1>Test</h1>, document.body);
---
+++ src/app/components/config.js
@@ -0,0 +1,41 @@
+export default {
+  featuredPosts: {
+    head: null,
+    boxes: [],
+  },
+
+  posts: [
+    {
+      name: 'Test post 1 name',
+      date: '2018-10-29T14:10:15.404Z',
+      author: 'TestAuthor 1',
+      group: 'Test Group 1',
+      headline: 'Test post 1 headline',
+      content: 'Test post 1 content',
+    },
+    {
+      name: 'Test post 2 name',
+      date: '2018-10-29T14:10:35.403Z',
+      author: 'TestAuthor 2',
+      group: 'Test Group 2',
+      headline: 'Test post 2 headline',
+      content: 'Test post 2 content',
+    },
+    {
+      name: 'Test post 3 name',
+      date: '2018-10-29T14:10:42.698Z',
+      author: 'TestAuthor 3',
+      group: 'Test Group 3',
+      headline: 'Test post 3 headline',
+      content: 'Test post 3 content',
+    },
+    {
+      name: 'Test post 4 name',
+      date: '2018-10-29T14:10:50.251Z',
+      author: 'TestAuthor 4',
+      group: 'Test Group 4',
+      headline: 'Test post 4 headline',
+      content: 'Test post 4 content',
+    },
+  ]
+}
